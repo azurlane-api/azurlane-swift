1f5b8389725408e81da1093780159d0f3804823e
KurozeroPB <root@kurozeropb.info>
2019-11-14 23:33:12 +0100

    Fix internal

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # azurlane-swift
 Wrapper for the unofficial azur lane json api in Swift
 
+## Install
+Import the library into your Package.swift
+`.package(url: "https://github.com/azurlane-api/azurlane-swift.git", .branch("master"))`
+
 ## Example
--- Sources/AzurLane/AzurLane.swift
+++ Sources/AzurLane/AzurLane.swift
@@ -7,10 +7,10 @@ public enum Category: String {
 }
 
 public struct AzurLane {
-    let version = "1.0.0"
+    public let version = "1.0.0"
     private let client: Client
 
-    init(_ userAgent: String? = nil) {
+    public init(_ userAgent: String? = nil) {
         if let ua = userAgent {
             self.client = Client(ua)
         } else {
@@ -18,19 +18,19 @@ public struct AzurLane {
         }
     }
 
-    func getShipBy(name: String, result: @escaping (Result<ShipResponse, AzurLaneAPIError>) -> Void) {
+    public func getShipBy(name: String, result: @escaping (Result<ShipResponse, AzurLaneAPIError>) -> Void) {
         client.fetchResource(.shipByName, params: [URLQueryItem(name: "name", value: name)], completion: result)
     }
 
-    func getShipBy(id: String, result: @escaping (Result<ShipResponse, AzurLaneAPIError>) -> Void) {
+    public func getShipBy(id: String, result: @escaping (Result<ShipResponse, AzurLaneAPIError>) -> Void) {
         client.fetchResource(.shipByID, params: [URLQueryItem(name: "id", value: id)], completion: result)
     }
 
-    func getShips(from category: Category, with value: String, result: @escaping (Result<ShipsResponse, AzurLaneAPIError>) -> Void) {
+    public func getShips(from category: Category, with value: String, result: @escaping (Result<ShipsResponse, AzurLaneAPIError>) -> Void) {
         client.fetchResource(.ships, params: [URLQueryItem(name: "orderBy", value: category.rawValue), URLQueryItem(name: category.rawValue, value: value)], completion: result)
     }
 
-    func getBuildInfo(from time: String, result: @escaping (Result<BuildResponse, AzurLaneAPIError>) -> Void) {
+    public func getBuildInfo(from time: String, result: @escaping (Result<BuildResponse, AzurLaneAPIError>) -> Void) {
         client.fetchResource(.construction, params: [URLQueryItem(name: "time", value: time)], completion: result)
     }
 
